68ac0c9023c9b73775190c89aa124da621b9d2a1
James Espinosa <de.james013@gmail.com>
2023-02-16 22:02:50 +0800

    fixed

--- Form1.vb
+++ Form1.vb
@@ -5,6 +5,7 @@
     Private Sub cbxValues_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxValues.SelectedIndexChanged
         txtSelectedItem.Text = cbxValues.Items(cbxValues.SelectedIndex)
     End Sub
+    ' ADD
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
         If txtDep.Text = "" Then
             MsgBox("Please insert a department!", MsgBoxStyle.Exclamation, "Empty input")
@@ -18,6 +19,8 @@
         txtDep.Clear()
         txtDep.Focus()
     End Sub
+
+    ' ADD AT INDEX AND INSERT
     Private Sub btnAddAt_Click(sender As Object, e As EventArgs) Handles btnAddAt.Click, btnInsert.Click
         If txtDep.Text = "" Then
             MsgBox("Please insert a department!", MsgBoxStyle.Exclamation, "Empty input")
@@ -34,6 +37,7 @@
         txtDep.Focus()
     End Sub
 
+    ' REMOVE
     Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
 
         If Not cbxValues.Items.Contains(txtDep.Text) Then
@@ -45,6 +49,7 @@
         txtDep.Focus()
     End Sub
 
+    ' REMOVE AT INDEX
     Private Sub btnRemoveAt_Click(sender As Object, e As EventArgs) Handles btnRemoveAt.Click
         If Not IsNumeric(txtIndex.Text) Then
             MsgBox("Please provide an valid index value!", MsgBoxStyle.Exclamation, "Invalid index")
@@ -65,10 +70,11 @@
     ' #################################################################################################
     ' LIST BOXES
     ' #################################################################################################
+    ' ADD
     Private Sub btnAdd1_Click(sender As Object, e As EventArgs) Handles btnAdd1.Click
 
-        If txtDep.Text = "" Then
-            MsgBox("Please insert a department!", MsgBoxStyle.Exclamation, "Empty input")
+        If txtStudent.Text = "" Then
+            MsgBox("Please insert a student!", MsgBoxStyle.Exclamation, "Empty input")
         ElseIf lsbValues.Items.Contains(txtStudent.Text) Then
             MsgBox("Student already exist!", MsgBoxStyle.Exclamation, "Data Exists")
             Return
@@ -78,6 +84,8 @@
         txtStudent.Clear()
         txtStudent.Focus()
     End Sub
+
+    ' REMOVE
     Private Sub btnRemove1_Click(sender As Object, e As EventArgs) Handles btnRemove1.Click
 
         If Not lsbValues.Items.Contains(txtStudent.Text) Then
@@ -89,6 +97,7 @@
         txtStudent.Focus()
     End Sub
 
+    ' SEARCH
     Private Sub btnSearch1_Click(sender As Object, e As EventArgs) Handles btnSearch1.Click
         If cbxValues.Items.Contains(txtStudent.Text) Then
             MsgBox("Student exist!", MsgBoxStyle.Information, "Data Found ")
